97de613ca95a6a9b2265d7ff2a0d115d8c2a32ca
dexterchan <dexterchan@gmail.com>
2022-02-07 23:47:02 +0800

    fix defualt lang

--- soup-base.setup.sh
+++ soup-base.setup.sh
@@ -5,6 +5,11 @@ sudo sed -i 's/# en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 sudo locale-gen
 
 #Then edit  /etc/default/locale 
+cat <<EOF > /tmp/locale 
+LANG=en_US.UTF-8
+LC_MESSAGES=en_US.UTF-8
+EOF
+sudo mv /tmp/locale /etc/default/locale 
 
 if [ -z "$host_name" ]; then
     host_name="k8s-node"
