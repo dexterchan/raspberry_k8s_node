9b1eaf8da7f04ab94e73821c01c689af093b910d
dexterchan <dexterchan@gmail.com>
2022-03-10 22:57:43 +0800

    update microk8s

--- install-microk8s.sh
+++ install-microk8s.sh
@@ -7,7 +7,7 @@ newgrp microk8s
 sudo ufw allow in on cni0 && sudo ufw allow out on cni0
 sudo ufw default allow routed
 
-microk8s enable dashboard dns registry istio
-microk8s enable ingress dns istio
+#microk8s enable dashboard dns regxistry istio
+microk8s enable ingress 
 
 echo 'alias mkubectl="microk8s kubectl"' >> ~/.bashrc
